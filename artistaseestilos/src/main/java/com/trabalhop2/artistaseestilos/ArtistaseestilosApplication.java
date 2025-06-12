35ab7c95316981806e26c9af8fd8adeef246ced9
maricruzd <mariana.sv1@hotmail.com>
2025-06-12 16:16:00 -0300

    Iniciando a Aplicação

---
+++ artistaseestilos/src/main/java/com/trabalhop2/artistaseestilos/ArtistaseestilosApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhop2.artistaseestilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistaseestilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistaseestilosApplication.class, args);
+	}
+
+}
---
+++ artistaseestilos/src/test/java/com/trabalhop2/artistaseestilos/ArtistaseestilosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhop2.artistaseestilos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistaseestilosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
